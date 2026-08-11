fe65245b02f1b5ee2a4f4b79fb409a106cb7f555
lealjo27 <lealjo27@gmail.com>
2026-08-11 14:30:37 +0200

    test sur render

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+
 from gradio_app import app
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+
     app.launch(
         server_name="0.0.0.0",
-        server_port=7860,
+        server_port=port,
         ssr_mode=False,
     )
