ebad8b4219d4befaaa64bc827230e4cdbc42fd49
lealjo27 <lealjo27@gmail.com>
2026-08-11 08:26:48 +0200

    Disable Gradio SSR

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from gradio_app import app
 
 if __name__ == "__main__":
-    app.launch()
+    app.launch(
+        server_name="0.0.0.0",
+        server_port=7860,
+        ssr_mode=False,
+    )
